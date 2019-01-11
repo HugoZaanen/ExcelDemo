8a5f6b8cc2fe77fb0b5f9d0ff5b5f3519ec5097d
Huego <hugozaanen@hotmail.com>
2019-01-11 15:33:54 +0100

    Add project files.

---
+++ ExcelDemo/ExcelDemo.WatchOSExtension/ExtensionDelegate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Foundation;
+using WatchKit;
+
+namespace ExcelDemo.WatchOSExtension
+{
+    public class ExtensionDelegate : WKExtensionDelegate
+    {
+        public ExtensionDelegate()
+        {
+        }
+    }
+}
+
